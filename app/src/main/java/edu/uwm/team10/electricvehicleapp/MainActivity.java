f9bcea845c7b81b72facd264175086c3de3203ba
Derek <derekpalecek@gmail.com>
2018-10-20 20:44:58 -0500

    Moved functionality around & got speed graph kind of working?

--- app/src/main/java/edu/uwm/team10/electricvehicleapp/MainActivity.java
+++ app/src/main/java/edu/uwm/team10/electricvehicleapp/MainActivity.java
@@ -1,7 +1,16 @@
 package edu.uwm.team10.electricvehicleapp;
 
 import android.Manifest;
+import android.support.v4.app.Fragment;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.ActivityCompat;
@@ -14,45 +23,37 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
+
+import models.TripDateModel;
+import models.TripModel;
+
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, SensorEventListener, LocationListener {
 
     private static final int MY_PERMISSION_ACCESS_FINE_LOCATION = 0;
+    final DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
 
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+
+    private TextView xAccel, yAccel, zAccel;
     private DrawerLayout mDrawerLayout;
     private Boolean isTripActive;
     private long tripStartTime;
     private long tripEndTime;
-    private float averageSpeed;
+    private double averageSpeed;
+    private double currentSpeed;
+    private ArrayList<Double> speedMeasurements;
+    private double startVolts;
+    private double endVolts;
 
-    public Boolean getTripActive() {
-        return isTripActive;
-    }
-
-    public void setTripActive(Boolean tripActive) {
-        isTripActive = tripActive;
-    }
-
-    public long getTripStartTime() {
-        return tripStartTime;
-    }
-
-    public long getTripEndTime() {
-        return tripEndTime;
-    }
-
-    public float getAverageSpeed() {
-        return averageSpeed;
-    }
-
-    public void setAverageSpeed(float averageSpeed) {
-        this.averageSpeed = averageSpeed;
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,18 +68,32 @@ public class MainActivity extends AppCompatActivity
                     MY_PERMISSION_ACCESS_FINE_LOCATION);
         }
 
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        //accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(this, accelerometer,
+                500000); // Call every half second (500k microseconds)
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         mDrawerLayout = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        xAccel = findViewById(R.id.xAccel);
+        yAccel = findViewById(R.id.yAccel);
+        zAccel = findViewById(R.id.zAccel);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawerLayout,
                 toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         mDrawerLayout.addDrawerListener(toggle);
         toggle.syncState();
         setTripActive(false);
+
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+
+        this.onLocationChanged(null);
 
         // Sets fragment to Home when opening. Rotating device and other odd movements will
         // reset the app to Home if there is not a null check
@@ -144,6 +159,7 @@ public class MainActivity extends AppCompatActivity
         if (!getTripActive()) {
             tripStartTime = System.nanoTime();
             setTripActive(true);
+            speedMeasurements = new ArrayList<>(); // When starting a new trip, wipe old measurements
         }
     }
 
@@ -151,7 +167,126 @@ public class MainActivity extends AppCompatActivity
         if (getTripActive()) {
             tripEndTime = System.nanoTime();
             setTripActive(false);
+            double elapsedTime = (getTripEndTime()
+                    - getTripStartTime()) / (1e+9);
+            TripDateModel tripDateModel = new TripDateModel(getTripStartTime(),getTripEndTime(),
+                    "Wed, Oct 18 2018");
+            TripModel tripModel = new TripModel(10.0, 1, 20,
+                    elapsedTime, getEndVolts(), 1, getStartVolts(), tripDateModel, speedMeasurements);
+            String pushString = mDatabase.child("trips").push().getKey();
+            mDatabase.child("trips").child(pushString).setValue(tripModel);
         }
     }
 
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.fragment_container);
+        if (fragment instanceof HomeFragment) {
+            HomeFragment homeFragment = (HomeFragment) fragment;
+            homeFragment.setXAccelText(" " + event.values[0]);
+            homeFragment.setYAccelText(" " + event.values[1]);
+            homeFragment.setZAccelText(" " + event.values[2]);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    public void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    public void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, accelerometer,
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.fragment_container);
+        if (fragment instanceof HomeFragment) {
+            HomeFragment homeFragment = (HomeFragment) fragment;
+            if (location == null) {
+                homeFragment.setSpeedText("-.- m/s");
+            } else {
+                float nCurrentSpeed = location.getSpeed();
+                homeFragment.setSpeedText(nCurrentSpeed + " m/s");
+                if (isTripActive) { addSpeedMeasurement(nCurrentSpeed); }
+            }
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+
+
+
+
+    // GETTERS AND SETTERS
+    public Boolean getTripActive() {
+        return isTripActive;
+    }
+
+    public void setTripActive(Boolean tripActive) {
+        isTripActive = tripActive;
+    }
+
+    public long getTripStartTime() {
+        return tripStartTime;
+    }
+
+    public long getTripEndTime() {
+        return tripEndTime;
+    }
+
+    public double getAverageSpeed() {
+        return averageSpeed;
+    }
+
+    public void setAverageSpeed(double averageSpeed) {
+        this.averageSpeed = averageSpeed;
+    }
+
+    public double getCurrentSpeed() { return currentSpeed; }
+
+    public void setCurrentSpeed(double currentSpeed) { this.currentSpeed = currentSpeed; }
+
+    public ArrayList<Double> getSpeedMeasurements() { return speedMeasurements; }
+
+    public void addSpeedMeasurement(double measurement) {
+        speedMeasurements.add(measurement);
+    }
+
+    public double getStartVolts() {
+        return startVolts;
+    }
+
+    public void setStartVolts(double startVolts) {
+        this.startVolts = startVolts;
+    }
+
+    public double getEndVolts() {
+        return endVolts;
+    }
+
+    public void setEndVolts(double endVolts) {
+        this.endVolts = endVolts;
+    }
 }
--- app/src/main/java/edu/uwm/team10/electricvehicleapp/SpeedFragment.java
+++ app/src/main/java/edu/uwm/team10/electricvehicleapp/SpeedFragment.java
@@ -1,6 +1,7 @@
 package edu.uwm.team10.electricvehicleapp;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,11 +9,62 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 public class SpeedFragment extends Fragment {
+
+    private final Handler mHandler = new Handler();
+    private Runnable mTimer1;
+    private View view;
+    private GraphView speedGraph;
+    private LineGraphSeries<DataPoint> mSeries1;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_speed, container, false);
+        view = inflater.inflate(R.layout.fragment_speed, container, false);
+
+        speedGraph = view.findViewById(R.id.speedGraph);
+        mSeries1 = new LineGraphSeries<>(generateSpeedData());
+        speedGraph.addSeries(mSeries1);
+
+        return view;
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mTimer1 = new Runnable() {
+            @Override
+            public void run() {
+                mSeries1.resetData(generateSpeedData());
+                mHandler.postDelayed(this, 500);
+            }
+        };
+        mHandler.postDelayed(mTimer1, 500);
+    }
+
+    @Override
+    public void onPause() {
+        mHandler.removeCallbacks(mTimer1);
+        super.onPause();
+    }
+
+
+    private DataPoint[] generateSpeedData() {
+        // int count = 30;
+        if (((MainActivity)getActivity()).getSpeedMeasurements() != null) {
+            int count = ((MainActivity) getActivity()).getSpeedMeasurements().size();
+            DataPoint[] speedValues = new DataPoint[count];
+            for (int i = 0; i < count; ++i) {
+                double x = i;
+                speedValues[i] = new DataPoint(i, ((MainActivity) getActivity()).getSpeedMeasurements().get(i));
+            }
+            return speedValues;
+        }
+        return new DataPoint[0];
+    }
+
 }
