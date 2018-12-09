ed1d85d1451cd9a2c48bd81a678497bc251c4d10
Derek <derekpalecek@gmail.com>
2018-12-09 13:27:56 -0600

    Last fixes

--- app/src/main/java/edu/uwm/team10/electricvehicleapp/MainActivity.java
+++ app/src/main/java/edu/uwm/team10/electricvehicleapp/MainActivity.java
@@ -2,6 +2,7 @@ package edu.uwm.team10.electricvehicleapp;
 
 import android.Manifest;
 import android.content.DialogInterface;
+import android.os.Handler;
 import android.os.SystemClock;
 import android.support.v4.app.Fragment;
 import android.content.Context;
@@ -72,6 +73,8 @@ public class MainActivity extends AppCompatActivity
     private double currentSpeed;
     private ArrayList<Double> speedMeasurements;
     private ArrayList<ArrayList<Double>> accelMeasurements;
+    private ArrayList<Double> recentAccelMeasurements;
+    private double recentSpeedMeasurement;
     private double startVolts;
     private double endVolts;
     private double distanceTraveled; // In meters
@@ -95,13 +98,13 @@ public class MainActivity extends AppCompatActivity
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this,
-                    new String[] { Manifest.permission.ACCESS_FINE_LOCATION },
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                     MY_PERMISSION_ACCESS_FINE_LOCATION);
         }
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        //accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION); // Linear Acceleration removes gravity
+//        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION); // Linear Acceleration removes gravity
         sensorManager.registerListener(this, accelerometer,
                 500000, 500000); // Call every half second (500k microseconds)
 
@@ -118,6 +121,22 @@ public class MainActivity extends AppCompatActivity
         mDrawerLayout.addDrawerListener(toggle);
         toggle.syncState();
         setTripActive(false);
+
+        new Handler().postDelayed(new Runnable() { // Add new accel measurements every second
+            @Override
+            public void run() {
+                addAccelMeasurement();
+                new Handler().postDelayed(this, 1000);
+            }
+        }, 500);
+
+        new Handler().postDelayed(new Runnable() { // Add new speed measurement every second
+            @Override
+            public void run() {
+                updateSpeedData();
+                new Handler().postDelayed(this, 1000);
+            }
+        }, 500);
 
         LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
@@ -257,47 +276,56 @@ public class MainActivity extends AppCompatActivity
         mDialog.show();
         mDialog.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(false);
 
-        vehicleSpinner.setOnItemSelectedListener(new OnItemSelectedListener()  {
+        vehicleSpinner.setOnItemSelectedListener(new OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 selectedVehicleName = vehicleList.get(position);
-                //Log.i(TAG, "Voltage: " + startVoltage.getText() + "  Vehicle: " + selectedVehicleName + "  Battery: " + selectedBatteryName);
                 if (selectedVehicleName != null && selectedBatteryName != null) {
-                    if (!selectedVehicleName.equals("Vehicles") && !selectedBatteryName.equals("Batteries")) {
+                    if (startVoltage.getText().length() > 0 && !selectedVehicleName.equals("Vehicles") && !selectedBatteryName.equals("Batteries")) {
                         mDialog.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(true);
+                    } else {
+                        mDialog.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(false);
                     }
                 }
             }
 
-            public void onNothingSelected(AdapterView<?> parent) { return; }
+            public void onNothingSelected(AdapterView<?> parent) {
+                return;
+            }
         });
         batterySpinner.setOnItemSelectedListener(new OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 selectedBatteryName = batteryList.get(position);
                 if (selectedVehicleName != null && selectedBatteryName != null) {
-                    if (!selectedVehicleName.equals("Vehicles") && !selectedBatteryName.equals("Batteries")) {
+                    if (startVoltage.getText().length() > 0 && !selectedVehicleName.equals("Vehicles") && !selectedBatteryName.equals("Batteries")) {
                         mDialog.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(true);
+                    } else {
+                        mDialog.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(false);
                     }
                 }
             }
 
-            public void onNothingSelected(AdapterView<?> parent) { }
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
         });
 
         startVoltage.addTextChangedListener(new TextWatcher() {
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
 
             @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
 
             @Override
             public void afterTextChanged(Editable s) {
-                Log.i(TAG, "Voltage: " + startVoltage.getText() + "  Vehicle: " + selectedVehicleName + "  Battery: " + selectedBatteryName);
                 if (selectedVehicleName != null && selectedBatteryName != null) {
                     if (startVoltage.getText().length() > 0 && !selectedVehicleName.equals("Vehicles") && !selectedBatteryName.equals("Batteries")) {
                         mDialog.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(true);
+                    } else {
+                        mDialog.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(false);
                     }
                 }
             }
@@ -319,7 +347,8 @@ public class MainActivity extends AppCompatActivity
             }
 
             @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {}
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
         };
         vehicleRef.addListenerForSingleValueEvent(vehicleEventListener);
 
@@ -331,14 +360,14 @@ public class MainActivity extends AppCompatActivity
                     String batteryName = snapshot.child("batteryName").getValue(String.class);
                     if (batteryName.equals(selectedBatteryName)) {
                         selectedBatteryId = snapshot.child("id").getValue(Integer.class);
-                        Log.i(TAG, "Battery id: " + selectedBatteryId);
                         break;
                     }
                 }
             }
 
             @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {}
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
         };
         batteryRef.addListenerForSingleValueEvent(batteryEventListener);
     }
@@ -392,34 +421,38 @@ public class MainActivity extends AppCompatActivity
 
         Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.fragment_container);
         if (fragment instanceof HomeFragment) {
+            Chronometer chron = ((HomeFragment) fragment).getChronometer();
+            chron.stop();
             ((HomeFragment) fragment).setStartButtonText("Start Trip");
         }
         toggleChronometer();
         double elapsedTime = (getTripEndTime()
                 - getTripStartTime()) / (1e+9);
         String date = new SimpleDateFormat("MM-dd-yyyy").format(new Date());
-        TripDateModel tripDateModel = new TripDateModel(getTripStartTime(),getTripEndTime(),
+        TripDateModel tripDateModel = new TripDateModel(getTripStartTime(), getTripEndTime(),
                 date);
         TripModel tripModel = new TripModel(calculateAverageSpeed(), selectedVehicleId,
                 selectedBatteryId, getDistanceTraveled(),
                 elapsedTime, getEndVolts(), getStartVolts(), tripDateModel, getSpeedMeasurements(),
                 getAccelMeasurements());
         String pushString = mDatabase.child("trips").push().getKey();
-        Log.i(TAG, "LOOK HERE: " + tripModel.getAverageSpeed());
         mDatabase.child("trips").child(pushString).setValue(tripModel);
     }
 
     /*
-    Accelerometer callback
+    Accelerometer callback. First we reset recentAccelMeasurements and add the new update. This is
+    called every time there is an accelerometer update (which is a lot), so we set a Handler in the
+    onCreate method to only add data to our "true" accelerometer data every half second.
      */
     @Override
     public void onSensorChanged(SensorEvent event) {
-        if (isTripActive) {
-            ArrayList<Double> measurements = new ArrayList<>();
-            measurements.add((double) event.values[0]);
-            measurements.add((double) event.values[1]);
-            measurements.add((double) event.values[2]);
-            addAccelMeasurement(measurements);
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            if (isTripActive) {
+                recentAccelMeasurements = new ArrayList<>();
+                recentAccelMeasurements.add((double) event.values[0]);
+                recentAccelMeasurements.add((double) event.values[1]);
+                recentAccelMeasurements.add((double) event.values[2]);
+            }
         }
     }
 
@@ -435,34 +468,38 @@ public class MainActivity extends AppCompatActivity
 
     public void onResume() {
         super.onResume();
-        sensorManager.registerListener(this, accelerometer,
-                SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                500000);
     }
 
     @Override
     public void onLocationChanged(Location location) {
         Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.fragment_container);
+        if (location != null) {
+            recentSpeedMeasurement = location.getSpeed();
+        } else {
+            recentSpeedMeasurement = 0.0;
+        }
         if (fragment instanceof HomeFragment) {
             HomeFragment homeFragment = (HomeFragment) fragment;
             if (location == null) {
                 homeFragment.setSpeedText("-.- m/s");
             } else {
-                float nCurrentSpeed = location.getSpeed();
-                homeFragment.setSpeedText(nCurrentSpeed + " m/s");
-                if (isTripActive) {
-                    updateSpeedData(nCurrentSpeed);
-                }
+                homeFragment.setSpeedText(recentSpeedMeasurement + " m/s");
             }
         }
     }
 
-    private void updateSpeedData(float currentSpeed)  {
-        addSpeedMeasurement(currentSpeed);
-        double addedDistance = currentSpeed * 1.0; // distance = speed * time
-        addDistance(addedDistance);
-        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.fragment_container);
-        if (fragment instanceof HomeFragment) {
-            ((HomeFragment) fragment).setDistanceTraveledText("" + getDistanceTraveled() + " m");
+    private void updateSpeedData() {
+        if (isTripActive) {
+            addSpeedMeasurement(recentSpeedMeasurement);
+            double addedDistance = recentSpeedMeasurement * 0.5; // distance = speed * time
+            addDistance(addedDistance);
+            Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.fragment_container);
+            if (fragment instanceof HomeFragment) {
+                ((HomeFragment) fragment).setDistanceTraveledText("" + getDistanceTraveled() + " m");
+            }
         }
     }
 
@@ -476,7 +513,7 @@ public class MainActivity extends AppCompatActivity
             }
             return (averageSpeed / speedMeasurements.size());
         } else {
-          return averageSpeed;
+            return averageSpeed;
         }
     }
 
@@ -486,6 +523,21 @@ public class MainActivity extends AppCompatActivity
             homeChronometer.start();
         } else {
             homeChronometer.stop();
+        }
+    }
+
+    /*
+    This starts the chronometer on the home fragment back up after a fragment change. Without this,
+    if the user changes fragments, the chronometer displayed would stick at 00:00.
+     */
+    public void setHomeFragmentChronometer() {
+        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.fragment_container);
+        if (fragment instanceof HomeFragment) {
+            if (isTripActive) {
+                Chronometer chron = ((HomeFragment) fragment).getChronometer();
+                chron.setBase(homeChronometer.getBase());
+                chron.start();
+            }
         }
     }
 
@@ -504,10 +556,9 @@ public class MainActivity extends AppCompatActivity
 
     }
 
-    private void addDistance(double distance) { distanceTraveled += distance; }
-
-
-
+    private void addDistance(double distance) {
+        distanceTraveled += distance;
+    }
 
 
     // GETTERS AND SETTERS
@@ -527,11 +578,17 @@ public class MainActivity extends AppCompatActivity
         return tripEndTime;
     }
 
-    public double getCurrentSpeed() { return currentSpeed; }
+    public double getCurrentSpeed() {
+        return currentSpeed;
+    }
 
-    public void setCurrentSpeed(double currentSpeed) { this.currentSpeed = currentSpeed; }
+    public void setCurrentSpeed(double currentSpeed) {
+        this.currentSpeed = currentSpeed;
+    }
 
-    public ArrayList<Double> getSpeedMeasurements() { return speedMeasurements; }
+    public ArrayList<Double> getSpeedMeasurements() {
+        return speedMeasurements;
+    }
 
     public void addSpeedMeasurement(double measurement) {
         speedMeasurements.add(measurement);
@@ -553,24 +610,29 @@ public class MainActivity extends AppCompatActivity
         this.endVolts = endVolts;
     }
 
-    public double getDistanceTraveled() { return distanceTraveled; }
+    public double getDistanceTraveled() {
+        return distanceTraveled;
+    }
 
-    public void setDistanceTraveled(double distance) { distanceTraveled = distance; }
+    public void setDistanceTraveled(double distance) {
+        distanceTraveled = distance;
+    }
 
     public ArrayList<ArrayList<Double>> getAccelMeasurements() {
         return accelMeasurements;
     }
 
-    public void addAccelMeasurement(ArrayList<Double> measurement) {
-        if (measurement != null) {
-            accelMeasurements.add(measurement);
+    public void addAccelMeasurement() {
+        if (recentAccelMeasurements != null) {
+            accelMeasurements.add(recentAccelMeasurements);
         }
     }
+
     //determines how many "bumps" we go over using accelerometer data.
     //takes a percent, if a 10% difference in vector is a bump, use .10 as input.
     //will need to change from all vectors to just the horizontal vector for bumpy roads
     //can use similar idea for changing direction frequently? Perhaps even frequent speed up and slow down horizontally
-    public void bumpyRoad(double percent){
+    public void bumpyRoad(double percent) {
 
 
         double percentPlus = 1 + percent;
@@ -578,35 +640,35 @@ public class MainActivity extends AppCompatActivity
 
         double[] absolute = new double[accelMeasurements.size()];//array used to store absolute vectors
 
-        for(int i = 0; i < accelMeasurements.size(); ++i){
+        for (int i = 0; i < accelMeasurements.size(); ++i) {
             double x = accelMeasurements.get(i).get(0);
             double y = accelMeasurements.get(i).get(1);
             double z = accelMeasurements.get(i).get(2);
 
             double a;//absolute value of all vectors, xyz
 
-            x = x*x;
-            y = y*y;
-            z = z*z;
+            x = x * x;
+            y = y * y;
+            z = z * z;
 
-            a = x+y+z;
+            a = x + y + z;
             a = Math.sqrt(a);
             a = a - 9.8;
             absolute[i] = a;
         }
 
         double avg = 0.0;
-        for (int i = 0; i < absolute.length; ++i){
+        for (int i = 0; i < absolute.length; ++i) {
             avg += absolute[i];
         }
         avg = avg / absolute.length;
 
-        for(int i = 0; i < absolute.length; ++i){
+        for (int i = 0; i < absolute.length; ++i) {
             //check if each absolute accelerometer vector is 10% less or 10% greater than average vector
-            if(absolute[i] < percentMinus){//if absolute vector at instance i is at least 10% less than avg, bump
+            if (absolute[i] < percentMinus) {//if absolute vector at instance i is at least 10% less than avg, bump
 
             }
-            if(absolute[i] > percentPlus){//if absolute vector at instance i is at least 10% greater than avg, bump
+            if (absolute[i] > percentPlus) {//if absolute vector at instance i is at least 10% greater than avg, bump
 
             }
         }
